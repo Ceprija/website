7e40165d308a42e5315148ce7a34e54bfb5e4ef0
Eli-ezer <2120100459@soy.utj.edu.mx>
2026-03-13 11:57:55 -0600

    templetes de correos

--- src/data/faculty.js
+++ src/data/faculty.js
@@ -16,7 +16,7 @@ export const faculty = [
         name: "Mtro. Manuel Espinoza Paredes",
         degree: "Maestro en Derecho",
         bio: "Especialista en Derecho Penal y Procesal con más de 30 años de experiencia en el Poder Judicial.",
-        fullBio: "El Mtro. Manuel Espinoza Paredes es Maestro en Derecho, especialista en Derecho Penal y Procesal, con más de 30 años de experiencia laboral en los juzgados del Poder Judicial. Cuenta con una Maestría en Orientación de Administración de Justicia y Seguridad Pública por el CUCSH, con la tesis: “La prisión preventiva en la Justicia Penal”.",
+        fullBio: "Maestro en Derecho, especialista en Derecho Penal y Procesal, con más de 30 años de experiencia laboral en los juzgados del Poder Judicial. Cuenta con una Maestría en Orientación de Administración de Justicia y Seguridad Pública por el CUCSH, con la tesis: “La prisión preventiva en la Justicia Penal”.",
         position_laboral: "Secretario Relator en la Sala del Supremo Tribunal de Justicia.",
         /* experience_institutional: [
             "UNIVER - Materia de Derecho Procesal Penal.",
